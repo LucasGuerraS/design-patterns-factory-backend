0dd75f053b81e57721b0f7d16a5de4ad4de0a683
LucasGuerraS <lucasgasardo@gmail.com>
2025-07-10 12:36:31 -0300

    feat: add initial files with spring config

---
+++ src/test/java/com/lucas/designfactorybackend/DesignFactoryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.designfactorybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignFactoryBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

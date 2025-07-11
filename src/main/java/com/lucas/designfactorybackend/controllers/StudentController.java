2864fef72cc9844ca3a3c5bd75f4bb729b41e949
LucasGuerraS <lucasgasardo@gmail.com>
2025-07-10 21:44:30 -0300

    feat: create CRUD for user

--- src/main/java/com/lucas/designfactorybackend/controllers/StudentController.java
+++ src/main/java/com/lucas/designfactorybackend/controllers/StudentController.java
@@ -4,10 +4,9 @@ import com.lucas.designfactorybackend.models.Student;
 import com.lucas.designfactorybackend.repositories.StudentRepository;
 import com.lucas.designfactorybackend.services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/student")
@@ -24,6 +23,36 @@ public class StudentController {
 
     @GetMapping
     public ResponseEntity<Iterable<Student>> findAll() {
-        return ResponseEntity.ok(studentRepository.findAll());
+        return ResponseEntity.ok(studentService.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<Student> save(@RequestBody Student student) {
+        studentService.save(student);
+        return new ResponseEntity<>(student, HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity<Student> update(@RequestBody Student student) {
+        return ResponseEntity.ok(studentService.update(student));
+    }
+
+    @DeleteMapping
+    @RequestMapping("/delete/{id}")
+    public ResponseEntity<Integer> delete(@PathVariable Integer id) {
+        studentService.delete(id);
+        return ResponseEntity.ok(id);
+    }
+
+    @GetMapping
+    @RequestMapping("/{id}")
+    public ResponseEntity<Student> findById(@PathVariable Integer id) {
+        return ResponseEntity.ok(studentService.findById(id));
+    }
+
+    @GetMapping
+    @RequestMapping("/email/{email}")
+    public ResponseEntity<Student> findByEmail(@PathVariable String email) {
+        return ResponseEntity.ok(studentService.findByEmail(email));
     }
 }
--- src/main/java/com/lucas/designfactorybackend/models/Student.java
+++ src/main/java/com/lucas/designfactorybackend/models/Student.java
@@ -1,9 +1,6 @@
 package com.lucas.designfactorybackend.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -22,6 +19,7 @@ public class Student {
 
     @Setter
     @Getter
+    @Column(nullable = false, unique = true)
     private String email;
 
     @Setter

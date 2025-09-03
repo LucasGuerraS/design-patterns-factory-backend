c4feda7061e8d357bc3d0d1f6ebbf315bc36fd2b
LucasGuerraS <lucasgasardo@gmail.com>
2025-09-03 11:58:40 -0300

    fix: config cors to all hosts and update compose.yml

--- src/main/java/com/lucas/designfactorybackend/controllers/StudentController.java
+++ src/main/java/com/lucas/designfactorybackend/controllers/StudentController.java
@@ -12,7 +12,6 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/student")
-@CrossOrigin(origins = {"http://localhost:3000" , "http://front:3000"})
 public class StudentController {
 
     @Autowired
